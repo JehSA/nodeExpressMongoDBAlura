105f5a67ee0dee7518b918d82a60a9a803f4dfa3
JehSA <jef.almeida@gmail.com>
2023-09-20 10:41:32 -0300

    Implementacao do CRUD autores.

--- src/app.js
+++ src/app.js
@@ -15,10 +15,4 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro deletado com sucesso!");
-});
-
 export default app;
