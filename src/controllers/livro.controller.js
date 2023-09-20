914bead5ffa342648e41fa4cdb4d41074f1b1937
JehSA <jef.almeida@gmail.com>
2023-09-20 20:51:06 -0300

    Insercao de busca por parametros em livros-editora

--- src/controllers/livro.controller.js
+++ src/controllers/livro.controller.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -23,9 +24,12 @@ class LivroController {
     };
 
     static async cadastrarLivro (req, res) {
+        const novoLivro = req.body;
         try{
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({ message: "Criado com sucesso!", livro: novoLivro });
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+            const livroCriado = await livro.create(livroCompleto);
+            res.status(201).json({ message: "Criado com sucesso!", livro: livroCriado });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha ao cadastrar livro.` });
         } 
@@ -49,6 +53,16 @@ class LivroController {
         } catch (erro) {
             res.status(500).json({ mnessage: `${erro.message} - Falha na requisição de exclusão do livro` });
         }        
+    };
+
+    static async listarLivrosPorEditora (req, res) {
+        const editora = req.query.editora;
+        try {
+            const livrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livrosPorEditora);
+        } catch (erro) {
+            res.status(500).json({ mnessage: `${erro.message} - Falha na busca!` });
+        }
     };
 
 };
