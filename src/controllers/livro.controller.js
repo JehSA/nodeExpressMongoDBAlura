b1316308bca455ccbf9c251e0df97ca0eb9cc603
JehSA <jef.almeida@gmail.com>
2023-09-27 16:17:05 -0300

    Avancando com buscas e filtros.

--- src/controllers/livro.controller.js
+++ src/controllers/livro.controller.js
@@ -1,5 +1,5 @@
-import { autor } from "../models/index.js";
-import { livro } from "../models/index.js";
+//import { autorSchema } from "../models/Autor.js";
+import { autor, livro } from "../models/index.js";
 
 class LivroController {
 
@@ -9,8 +9,7 @@ class LivroController {
       res.status(200).json(listaLivros);
     } catch (erro) {
       next(erro);
-    }
-            
+    }            
   }
 
   static async listarLivroPorId (req, res, next) {
@@ -56,33 +55,54 @@ class LivroController {
   }
 
   static async listarLivrosPorFiltro (req, res, next) {
-
-    const busca = processaBusca(req.query);        
-
+    const busca = await processaBusca(req.query);
     try {      
-      const livrosPorEditora = await livro.find(busca);
-      res.status(200).json(livrosPorEditora);
+      if(busca !== null) {
+        const livrosPorEditora = await livro
+          .find(busca)
+          .populate("autor");
+        res.status(200).json(livrosPorEditora);
+      } else {
+        res.status(200).send([]);
+      }      
     } catch (erro) {
       next(erro);
     }
   }
 }
 
-function processaBusca(parametros) {
-  const { editora, titulo, minPaginas, maxPaginas } = parametros;
-  const busca = {};
+async function processaBusca(parametros) {
+  const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = parametros;
+  let busca = {};
 
   if(editora) busca.editora = { $regex: editora, $options: "i" }; 
   if(titulo) busca.titulo = { $regex: titulo, $options: "i" };
 
   /* O bloco de código abaixo define parametros de busca para quantidades
-     minimas e maximas de páginas...
+     minimas e maximas de páginas...z
   */
   if(minPaginas || maxPaginas) busca.paginas = {};
   // gte = Greater Than or Equal = Maior ou igual a..
   if(minPaginas) busca.paginas.$gte = minPaginas;
-  // gte = LessThan or Equal = Menor ou igual a...
+  // lte = Less Than or Equal = Menor ou igual a...
   if(maxPaginas) busca.paginas.$lte = maxPaginas;
+
+  /*
+    A busca por autor não funciona como esperado, talvez tenha a ver com o autorSchema
+    da model Autor.js. Não sei como se faz os filtros por lá, depois tenho que pesquisar.
+    Esse aspecto do projeto está um pouco diferente do apresentado no curso!!!!!!!
+  */
+  if(nomeAutor) {
+    const autorN = await autor.findOne({ nome: nomeAutor });
+    console.log(autorN, "Autor NNNNNNNNNNNNN");
+
+    if(autorN !== null) {
+      busca.autor = autorN._id;
+      console.log(busca.autor, "!!!!!!!!!!!");
+    } else {
+      busca = null;
+    }
+  }
 
   return busca;
 }
